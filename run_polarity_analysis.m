3e7f42e54b784653ee028e4eab37865f6f88bb6b
endaohan <84255432+endaohan@users.noreply.github.com>
2025-01-20 22:15:58 +0800

    Add files via upload

---
+++ run_polarity_analysis.m
@@ -0,0 +1,14 @@
+% Run velocity and polarity analysis for the polarity data
+% Written by Endao Han, V1
+
+clear
+close all
+
+fileName = 'ExampleData'; 
+
+fun_velocity( fileName )
+fun_polarity( fileName )
+close all
+
+
+
